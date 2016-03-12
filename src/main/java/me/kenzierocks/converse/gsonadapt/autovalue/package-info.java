0160a57d097493ea75e07e28112a5f07fce865af
Kenzie Togami <ket1999@gmail.com>
2016-03-11 22:40:04 -0800

    Clear up license info.

--- src/main/java/me/kenzierocks/converse/gsonadapt/autovalue/package-info.java
+++ src/main/java/me/kenzierocks/converse/gsonadapt/autovalue/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Based off of https://gist.github.com/JakeWharton/0d67d01badcee0ae7bc9.
- * Licensed under Apache 2.
+ * Based off of https://gist.github.com/JakeWharton/0d67d01badcee0ae7bc9, which
+ * is licensed under Apache 2.
  */
 package me.kenzierocks.converse.gsonadapt.autovalue;
